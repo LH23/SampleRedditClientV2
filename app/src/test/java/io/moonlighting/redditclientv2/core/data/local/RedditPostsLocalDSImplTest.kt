9489959f91ed653ec0e73293b233905303561be4
LH23 <lh@moonlighting.io>
2023-08-12 20:01:06 -0300

    added Log class mock

--- app/src/test/java/io/moonlighting/redditclientv2/core/data/local/RedditPostsLocalDSImplTest.kt
+++ app/src/test/java/io/moonlighting/redditclientv2/core/data/local/RedditPostsLocalDSImplTest.kt
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 
 import org.junit.jupiter.api.Test
-import java.util.logging.Level.CONFIG
 
 
 class RedditPostsLocalDSImplTest {
