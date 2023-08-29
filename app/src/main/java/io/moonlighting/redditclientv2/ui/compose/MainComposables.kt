9d537d543e2f241908d05422fa395be5d9aee7e9
LH23 <lh@moonlighting.io>
2023-08-29 04:48:09 -0300

    Added composables for the PostDetailsScreen view basic layout

--- app/src/main/java/io/moonlighting/redditclientv2/ui/compose/MainComposables.kt
+++ app/src/main/java/io/moonlighting/redditclientv2/ui/compose/MainComposables.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -35,8 +37,11 @@ import androidx.paging.compose.itemKey
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import io.moonlighting.redditclientv2.R
+import io.moonlighting.redditclientv2.ui.compose.postslist.RedditPostType
 import io.moonlighting.redditclientv2.ui.compose.postslist.UIRedditPost
 import kotlinx.coroutines.flow.Flow
+
+
 
 @Composable
 internal fun ListOfPosts(redditPostsFlow: Flow<PagingData<UIRedditPost>>, onPostClick: (UIRedditPost) -> Unit) {
@@ -60,7 +65,7 @@ internal fun ListOfPosts(redditPostsFlow: Flow<PagingData<UIRedditPost>>, onPost
                 LaunchedEffect(key1 = post) {
                     println("post $index added to the UI: $post")
                 }
-                RedditPostCard(post, Modifier, onPostClick)
+                RedditListPostCard(post, Modifier, onPostClick)
             } else {
                 RedditPostCardPlaceholder()
             }
@@ -126,9 +131,9 @@ fun RedditPostCardPlaceholder(
 
 @OptIn(ExperimentalGlideComposeApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun RedditPostCard(post: UIRedditPost,
-                   modifier: Modifier = Modifier,
-                   onPostClick: (UIRedditPost) -> Unit
+fun RedditListPostCard(post: UIRedditPost,
+                       modifier: Modifier = Modifier,
+                       onPostClick: (UIRedditPost) -> Unit
 ) {
     ElevatedCard(modifier = modifier
         .fillMaxWidth()
@@ -163,15 +168,99 @@ fun RedditPostCard(post: UIRedditPost,
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun RedditDetailsPostCard(post: UIRedditPost,
+                          modifier: Modifier = Modifier
+) {
+    ElevatedCard(modifier = modifier
+        .fillMaxWidth()
+    ) {
+        Column {
+            PostCardHeader(post)
+            when (post.type) {
+                RedditPostType.IMAGE_POST -> { FullSizedImage(post.url) }
+//                VIDEO_POST -> {}
+//                WEB_POST -> {}
+//                TEXT_POST -> {}
+                else -> { ErrorMessage(stringResource(R.string.error_invalid_post)) }
+            }
+            PostCardFooter(post)
+        }
+    }
+}
+
+@Composable
+fun PostCardHeader(post: UIRedditPost, modifier: Modifier = Modifier) {
+    Row (modifier = modifier.fillMaxWidth().height(56.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        SubredditIcon(post.subredditIconUrl)
+        Column (modifier = Modifier.weight(1f)){
+            Text(text = post.subredditPrefixed)
+            Text(text = post.authorPrefixed)
+        }
+        Text(text = post.creationDateFormatted)
+    }
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun SubredditIcon(iconUrl: String, modifier: Modifier = Modifier) {
+    GlideImage(
+        model = iconUrl,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(56.dp),
+        contentDescription = null
+    )
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun FullSizedImage(imageUrl: String, modifier: Modifier = Modifier) {
+    GlideImage(
+        model = imageUrl,
+        contentScale = ContentScale.Crop,
+        modifier = modifier.fillMaxWidth().height(400.dp),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun PostCardFooter(post: UIRedditPost, modifier: Modifier = Modifier) {
+    Row (
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ){
+        Button(content = { Text("Share")}, onClick = {})
+        Button(content = { Text("Save")}, onClick = {})
+        Button(content = { Text("Open")}, onClick = {})
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun RedditPostCardPreview() {
-    val post = UIRedditPost(
-        "00_asdcvb",
-        "This is a test post with something very amazing",
-        "r/amazed", "u/lio23",
-        "https://freeiconshop.com/wp-content/uploads/edd/reddit-flat.png",
-        "https://freeiconshop.com/icon/reddit-icon-flat/"
-    )
-    RedditPostCard(post) { p -> Log.d("preview", "$p") }
+    val post = fakePost
+    RedditDetailsPostCard(post)
 }
+
+@Preview(showBackground = true)
+@Composable
+fun RedditListPostCardPreview() {
+    val post = fakePost
+    RedditListPostCard(post) { p -> Log.d("preview", "$p") }
+}
+
+val fakePost = UIRedditPost(
+    "00_asdcvb",
+    "This is a test post with something very amazing",
+    "r/amazed",
+    "https://freeiconshop.com/wp-content/uploads/edd/reddit-flat.png",
+    "u/lio23",
+    "https://freeiconshop.com/wp-content/uploads/edd/reddit-flat.png",
+    "https://freeiconshop.com/icon/reddit-icon-flat/",
+    RedditPostType.IMAGE_POST,
+    "12/8/2023 03:55pm"
+)
