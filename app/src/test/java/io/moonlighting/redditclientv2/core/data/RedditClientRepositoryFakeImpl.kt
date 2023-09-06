a157f119445c175856fb529b06c08c337392b5a9
LH23 <lh@moonlighting.io>
2023-09-06 04:18:13 -0300

    Updated redditPost repo tests

--- app/src/test/java/io/moonlighting/redditclientv2/core/data/RedditClientRepositoryFakeImpl.kt
+++ app/src/test/java/io/moonlighting/redditclientv2/core/data/RedditClientRepositoryFakeImpl.kt
@@ -1,6 +1,7 @@
 package io.moonlighting.redditclientv2.core.data
 
 import android.util.Log
+import android.widget.DatePicker
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
@@ -11,6 +12,8 @@ import io.moonlighting.redditclientv2.core.data.model.RedditPost
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.onEach
+import java.time.Instant
+import java.util.Date
 
 class RedditClientRepositoryFakeImpl: RedditClientRepository {
 
@@ -39,13 +42,17 @@ class RedditClientRepositoryFakeImpl: RedditClientRepository {
         ).flow
     }
 
+    override fun getRedditPost(fullname: String): Flow<RedditPost> {
+        TODO("Not yet implemented")
+    }
+
     companion object {
         const val errorMessage = "Test Exception"
         val fakePosts: List<RedditPost> = listOf(
-            RedditPost("1", "Hello im a reddit post1", "r/test", "u/lio", "", ""),
-            RedditPost("2", "Hello im a reddit post2", "r/test", "u/lio", "", ""),
-            RedditPost("3", "Hello im a reddit post3", "r/test", "u/lio", "", ""),
-            RedditPost("4", "Hello im a reddit post4", "r/test", "u/lio", "", "")
+            RedditPost("1", "Hello im a reddit post1", "r/test", "u/lio", "", "", "", "", "", "", Date.from(Instant.now())),
+            RedditPost("2", "Hello im a reddit post2", "r/test", "u/lio", "", "", "", "", "", "", Date.from(Instant.now())),
+            RedditPost("3", "Hello im a reddit post3", "r/test", "u/lio", "", "", "", "", "", "", Date.from(Instant.now())),
+            RedditPost("4", "Hello im a reddit post4", "r/test", "u/lio", "", "", "", "", "", "", Date.from(Instant.now()))
         )
     }
 }
